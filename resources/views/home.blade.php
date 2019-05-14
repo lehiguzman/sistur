5ddf1aac3a3b306d9db35b9815ac21170888d925
Lehi Guzman <lehyjoseph2007@gmail.com>
2019-05-14 09:25:49 -0400

    inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SISTUR-INICIO</title>
+	<link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/x-icon"/>
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/estilos.css') }}">	
+</head>
+<body>
+	<div class="content">
+		<ul class="slider">
+			<li class="img1"></li>
+			<li class="img2"></li>			
+			<li class="img3"></li>
+			<li class="img4"></li>
+			<li class="img5"></li>			
+		</ul>
+	</div>
+</body>
+</html>
