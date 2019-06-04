93a0377a37c1681d1fe855a5313419498cc5ecc3
Lehi Guzman <lehyjoseph2007@gmail.com>
2019-06-04 15:52:01 -0400

    Actualizacion de fondos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-6">
-            <div class="card bg-blue">
+            <div class="card bg-opacity">
                 <div class="card-header text-center">{{ __('Login') }}</div>
 
                 <div class="card-body">
